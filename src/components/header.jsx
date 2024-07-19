b2a3fa6b32f2623e73139e13ac26d2dda19e4c50
Jesus Retana Artavia <jesus@Jesuss-MacBook-Air.local>
2024-07-18 22:56:24 -0600

    img size

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
+import globe from "../imgs/glove.png"
 export default 
 
 function Header() {
    return(
     <header className="header">
-    <img className="logo-glove" src="./src/imgs/glove.png" alt="">
+    <img className="logo-glove" src={globe} alt="">
     </img> <h1>my travel journal.</h1>
  </header>
    )
